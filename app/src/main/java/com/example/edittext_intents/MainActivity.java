727d3bf9b03ccec2464210c8670125f8f4fbb74f
ml3424 <ml3424@bs.amalnet.k12.il>
2024-04-01 21:10:59 +0300

    all the btns except the credits

--- app/src/main/java/com/example/edittext_intents/MainActivity.java
+++ app/src/main/java/com/example/edittext_intents/MainActivity.java
@@ -3,12 +3,165 @@ package com.example.edittext_intents;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button plus, minos, mult, dev, aC, eq, credits;
+    EditText eT;
+    Toast t;
+
+    double num = 0;
+    double result = 0;
+    char action = ' ';
+    String strNum;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eT = findViewById(R.id.eT);
+
+        plus = findViewById(R.id.plus);
+        minos = findViewById(R.id.minos);
+        mult = findViewById(R.id.mult);
+        dev = findViewById(R.id.dev);
+        aC = findViewById(R.id.aC);
+        eq = findViewById(R.id.eq);
+        credits = findViewById(R.id.credits);
+
+    }
+
+    public void plusClick(View view) {
+        strNum = eT.getText().toString();
+        if(strNum != "")
+        {
+            num = Double.parseDouble(strNum);
+            eT.getText().clear();
+
+            if(action != ' ')
+            {
+                result = cal(num, result, action);
+            }
+            else
+            {
+                result = num;
+            }
+            action = '+';
+        }
+
+    }
+
+    public void minosClick(View view) {
+        strNum = eT.getText().toString();
+        if(strNum != "")
+        {
+            num = Double.parseDouble(strNum);
+            eT.getText().clear();
+
+            if(action != ' ')
+            {
+                result = cal(num, result, action);
+            }
+            else
+            {
+                result = num;
+            }
+            action = '-';
+        }
+    }
+
+    public void multiClick(View view) {
+        strNum = eT.getText().toString();
+        if(strNum != "")
+        {
+            num = Double.parseDouble(strNum);
+            eT.getText().clear();
+
+            if(action != ' ')
+            {
+                result = cal(num, result, action);
+            }
+            else
+            {
+                result = num;
+            }
+            action = '*';
+        }
+    }
+
+    public void devClick(View view) {
+        strNum = eT.getText().toString();
+        if(strNum != "")
+        {
+            num = Double.parseDouble(strNum);
+            eT.getText().clear();
+
+            if(action != ' ')
+            {
+                result = cal(num, result, action);
+            }
+            else
+            {
+                result = num;
+            }
+            action = '/';
+        }
+    }
+
+    public void acClick(View view) {
+        eT.getText().clear();
+        result = 0;
+        action = ' ';
+    }
+
+    public void eqClick(View view) {
+        // get the number from the eT and clear it
+        strNum = eT.getText().toString();
+        if(strNum != "")
+        {
+            num = Double.parseDouble(strNum);
+            eT.getText().clear();
+
+            //cal the result
+            if(action != ' ')
+            {
+                result = cal(num, result, action);
+            }
+
+            // show the result on the eT
+            String st = Double.toString(result);
+            eT.setText(st);
+
+            action = ' ';
+        }
+
+    }
+
+    public void creditsClick(View view) {
+    }
+
+    public double cal(double newNum,double oldNum, char action) {
+        if (action == '+') {
+            return oldNum + newNum;
+        }
+        else if(action == '-')
+        {
+            return oldNum - newNum;
+        }
+        else if(action == '*')
+        {
+            return oldNum * newNum;
+        }
+        else
+        {
+            return oldNum / newNum;
+        }
     }
 }
+
